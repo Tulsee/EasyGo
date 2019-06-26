2e63d852891fbc0e8993b6a7ca47bbceff75b2da
tulsee <ramtulsi022@gmail.com>
2019-06-26 13:48:28 +0545

    css and js is added

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const exphbs  = require('express-handlebars');
+const exphbs = require("express-handlebars");
+const path = require("path");
 const app = express();
 
 //middlewar for handlebars
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
+//set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 const PORT = 5000;
 app.listen(PORT, () => {
